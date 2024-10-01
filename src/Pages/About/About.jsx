004e1d89397453d69ae472bb2d5c0c37b3d6bf6b
Rithish06 <rithish.manohar006@gmail.com>
2024-10-01 12:30:01 +0530

    service page pesponsive

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -22,14 +22,14 @@ const About = () => {
         {/* container 2 */}
 
         <div className="about_container_2">
-            <div className="container_2_box">
+            <div className="container_2_box container_2_box_1">
                 <div className="box_1_heading">What<br /><span>we</span><br />do</div>
                 <div className="box_content">
                     <div className="para_1">The Invention Minds team comes with a unique strategy, and our market analyses are accurate and subject-oriented. We are best at providing consistently successful strategies and unique, forward-thinking digital marketing, web development, and website design solutions that consider each client’s individual requirements and unique demands.</div>
                     <div className="para_1">Our team of specialists and strong knowledge base allow us to take any project from inception, through requirements and design, to implementation and installation. For each step in the execution of the project, we do our best to perform tasks with responsibility by using knowledge, creativity, and skills to the utmost efficiency.</div>
                 </div>
             </div>
-            <div className="container_2_box">
+            <div className="container_2_box container_2_box_2">
                 <div className="box_content">
                     <div className="para_1">We value open communication and are always delighted to engage in friendly discussions to address questions, concerns, or ideas from our customers, aiming to reach the best decisions together. Client satisfaction is our primary driving force, and we pride ourselves on delivering promises while acknowledging and rectifying any mistakes. Upholding business ethics and trust is paramount to achieving stable, ongoing growth. The basic ideals at Invention Minds are quality over quantity, context over substance, and accuracy over production. Our commitment to achieving optimization in all processes aligns with the fundamental factors of success: Quality, Time, and Cost.</div>
                 </div>
